dc9a2c771d945c777b80c38f58a2ffcbf8aad37e
Ana Wagner <anawagner1138@gmail.com>
2018-12-04 22:01:13 -0600

    Update 04/04.py

--- 04/04.py
+++ 04/04.py
@@ -1,6 +1,8 @@
 import re
 from operator import itemgetter
 
+# returns list of tuples (month,day,hour,minute,message entry)
+# sorted by month, day, hour, and minute in that order
 def get_data(file):
     infile = open(file,"r")
     data = infile.read()
@@ -8,6 +10,7 @@ def get_data(file):
     entries = re.findall(r'\[\d+-(\d+)-(\d+)\s(\d+):(\d+)\]\s(.*)\n',data)
     return sorted(entries, key=itemgetter(0,1,2,3))
 
+# print functions for visualizing and checking data
 def print_data(data):
     counter = 0
     for entry in data:
@@ -17,15 +20,9 @@ def print_data(data):
         print("")
     print("There are", counter, "entries")
 
-def print_naps(naps):
-    counter = 0
-    for nap in naps:
-        print(nap)
-        print("")
-        counter += 1
-    print("there are:",counter,"naps")
-
-def get_all_guard_naps(data):
+# returns dictionary of data for each nap ie:
+# {'sleep': 19, 'duration': 4, 'wake': 23, 'id': '1637'}
+def get_all_naps(data):
     guard_naps = []
     for entry in data:
         if re.search(r'Guard',entry[4]):
@@ -38,58 +35,52 @@ def get_all_guard_naps(data):
             guard_naps.append(nap)
     return guard_naps
 
+# returns a dictionary of key = guard id, value = total sleep time,
+# one entry per guard
 def guard_totals(naps):
     guard_total = {}
     for nap in naps:
         guard_total[nap['id']] = nap['duration'] + guard_total.get(nap['id'],0)
     return guard_total
 
+#returns id of the guard with the most sleep time
 def sleepiest_guard(naps):
     totals = guard_totals(naps)
     return max(totals, key=totals.get)
 
+#returns all the naps for one guard
 def get_one_guard_naps(naps, id):
-    sleepy_guard_naps = []
-    for nap in naps:
-        if nap['id'] == id:
-            sleepy_guard_naps.append(nap)
-    return sleepy_guard_naps
+    return list(filter(lambda nap: nap['id'] in id, naps))
 
-def minute_counts(naps):
-    sleep_min = {}
+# returns the minute most naped from a set of given guard naps
+# in a tuple with the count  (minute, count)
+def max_nap_minute(naps):
+    minute_sleep_counts = {}
     for nap in naps:
         for n in range(nap['sleep'],nap['wake']):
-            sleep_min[n] = sleep_min.get(n,0) + 1
-    return sleep_min
+            minute_sleep_counts[n] = minute_sleep_counts.get(n,0) + 1
+    minute = max(minute_sleep_counts, key=lambda k: minute_sleep_counts[k])
+    return (minute, minute_sleep_counts[minute])
 
-def sleepiest_time(naps):
-    min_counts = minute_counts(naps)
-    minute = max(min_counts, key=lambda k: min_counts[k])
-    count = min_counts[minute]
-    return (minute,count)
-
-def consistent_sleep_minute(naps):
+# returns (id, minute, count) of the minute most slept by any one guard
+def most_napped_minute_any_guard(naps):
     list_of_max = []
     for id in guard_totals(naps).keys():
-        min_count = sleepiest_time(get_one_guard_naps(naps,id))
+        min_count = max_nap_minute(get_one_guard_naps(naps,id))
         list_of_max.append( (id, min_count[0], min_count[1]))
     return max(list_of_max, key=itemgetter(2))
 
 def main():
     filename = "input.txt"
     the_data = get_data(filename)
-    #print_data(the_data)
-    naps = get_all_guard_naps(the_data)
-    #print_naps(naps)
+    naps = get_all_naps(the_data)
     the_sleepy_guard = sleepiest_guard(naps)
     print("Most sleeping guard: ", the_sleepy_guard)
-    sleepy_minute = sleepiest_time(get_one_guard_naps(naps, the_sleepy_guard))
+    sleepy_minute = max_nap_minute(get_one_guard_naps(naps,the_sleepy_guard))[0]
     print("Most sleepy minute for sleepiest guard: ", sleepy_minute)
-    answer = int(the_sleepy_guard) * int(sleepy_minute[0])
-    print("answer 1: ", answer)
-    consistent_sleepy = consistent_sleep_minute(naps)
+    print("Answer1: ", int(the_sleepy_guard) * sleepy_minute)
+    consistent_sleepy = most_napped_minute_any_guard(naps)
     print("consitent sleepy time for one guard", consistent_sleepy)
-    answer2 = int(consistent_sleepy[0]) * int(consistent_sleepy[1])
-    print("answer 2: ", answer2)
+    print("Answer2: ",int(consistent_sleepy[0]) * consistent_sleepy[1])
 
 main()
